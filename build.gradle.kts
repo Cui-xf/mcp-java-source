043c57deb9e1756dcccd85e2da7a6097f421bc45
cxf <cuixiaofeng@yuanfudao.com>
2025-08-21 19:38:52 +0800

    ADD:icon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    id("java")
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
 }
